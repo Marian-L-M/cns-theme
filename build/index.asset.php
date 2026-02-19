6b81204e62d27a7adf918070ee88e1cd1ff26b60
Marian-L-M <marianlmaschke@gmail.com>
2026-02-20 08:28:40 +0900

    adjust project sturcture to block.json notation

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '414caf0092e799aba343');
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const path = require('path');
-const glob = require('fast-glob');
-
-// Auto-discover all block JS files
-const blockEntries = glob.sync('./blocks/*.js').reduce((entries, file) => {
-    const name = path.basename(file, '.js');
-    entries[`blocks/${name}`] = file;
-    return entries;
-}, {});
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
 
 module.exports = {
-    ...defaultConfig,
-    entry: {
-        index: './src/index.js',
-        ...blockEntries,
-    },
+  ...defaultConfig,
+  entry: {
+    index: "./src/index.js",
+    ...blockEntries,
+  },
 };
